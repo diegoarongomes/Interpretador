15959e92403426ca8f20d7d6f237285e4a9b4f2e
Dionatan Gomes <dionatangomes08@gmail.com>
2014-09-29 11:14:17 -0300

    Criação de variável implementada. Setar valor de variável definido. Melhoria no método de busca da variável criada.
    Signed-off-by: Dionatan Gomes <dionatangomes08@gmail.com>

--- Fonte/Interpretador.java
+++ Fonte/Interpretador.java
@@ -1,29 +1,32 @@
+//Classe Interpretador.
+//Esta classe recebe o vetor de strings da Main, e faz a decodificação linha por linha, em busca dos comandos a serem realizados. Ao encontrar algum comando válido
+//chama a classe Variáveis, que trata as informações.
 class Interpretador{
 	private String arquivo[];
 	private Variaveis v[];
 	
+	//Incialização do objeto
 	public Interpretador(){
 		this.v = new Variaveis[20];
 	}
 	
+	//Método que faz a leitura linha por linha.
 	public void interpretaCod (String linhas[]){
 		int i,pos;
-		pos=0;
+		pos=0;//Será o indice do vetor de variáveis.
 		this.arquivo = linhas;
 		
-		for (i=0;i<linhas.length;i++){
-			if (linhas[i].contains("var")){
-				v[pos].criaVariavel(linhas[i]);
+		for (i=0;i<=arquivo.length;i++){
+			//Dentro do laço, selecionamos uma linha por vez para interpretar, em busca de Totens que auxiliem nos processos..
+			//1° SE. Caso a inha contenha a palavra Var, entende-se como criação de variável do tipo Var:a;
+			if (arquivo[i].contains("Var")){
+				v.criaVariavel(arquivo[i],v[],pos);
+				pos++;
 			}
-			
-			else if (linhas[i].contains("+")||linhas[i].contains("-")||linhas[i].contains("/")||linhas[i].contains("*")){
-				
-			}
-			
-			else if (linhas[i].contains("=")){
-			
-			}
-		
+			//2° SE. Caso a linha possua atribuição de valor do tipo a=2;
+			else if (arquivo[i].contains("=")){
+				v.setValor(arquivo[i],v[],pos);
+			}		
 		}
 		
 		
--- Fonte/Variaveis.java
+++ Fonte/Variaveis.java
@@ -1,3 +1,5 @@
+//Classe Variáveis, responsável por criar cada variável, setar e pegar valores do objeto variaveis,
+//além de possuir os metodos que executam os comandos do código interpretado.
 class Variaveis{
 	
 	private String nome;
@@ -5,36 +7,49 @@ class Variaveis{
 	
 	public Variaveis(){
 		this.nome = "0";
-		this.valor = 0;
+		this.valor = 0.0;
 	}
 	
 	public void setNome(String a){
 		this.nome = a;
 	}
+	
 	public String getNome(){
 		return this.nome;
 	}
-	public void setValor(double a){
-		this.valor = a;
+	//Método para inserir valores em uma variável. O método chama o método nomePesquisa para encontrar o indice da variável no vetor,
+	//encontra o numero na string, transforma em double, e coloca na variável valor do objeto v[i]
+	public void setValor(String s,Variaveis vet[],int p){
+		int j;
+		double k;
+		String nome = new String();
+		j = s.indexOf("=");		
+		nome = s.substring(0,j);		
+		int i = this.nomePesquisa(vet,p,nome);
+		k = Double.parseDouble(s.substring(j+1,s.indexOf(";")));
+		this[i].valor = k;
 	}
+
 	public double getValor(){
 		return this.valor;
 	}
-	public int nomePesquisa(Variaveis v[],int p, String n){
+	//Método de pesquisa, recebe um vetor de variáveis, um int com o tamanho do vetor, 
+	//e uma string a ser procurada. Retorna o indice do vetor em que esta a variável
+	//ou -1 caso não encontre a string no vetor.
+	public int nomePesquisa(Variaveis v[],int p, String s){
 		int i;
 		for (i=0;i<p;i++){
-			if (v[i].nome == n){
+			if (v[i].nome.compareTo(s) == 0){
 				return i;
 			}
 		}
 		return -1;
 	}
-	public void criaVariavel(String l){
+	//Método de criação de nova variável. 
+	public void criaVariavel(String l,Variaveis v[],int pos){
 		int i,j,k;
 		i=l.lastIndexOf(";");
-		for (j=i;(l.charAt(j)) != null ;j--){
-			k=j;
-		}
-		this.nome = l.substring(k,i-1);
+		k=l.lastIndexOf(":");
+		this.nome = l.substring(k+1,i);		
  	}
 }
